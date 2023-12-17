577276a25fd11becbaf7dc1d84ae524a8fdd431a
GivenBY <suryakantupadhyay18@gmail.com>
2023-12-17 18:28:40 +0530

    feat: landing page route

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,17 @@
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
+import { LandingFooter } from "@/components/landing-footer";
+
+const LandingPage = () => {
+  return (
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
+      <LandingFooter />
+    </div>
+  );
+};
+
+export default LandingPage;
