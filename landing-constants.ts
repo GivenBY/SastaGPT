afada838772572af64e53c80e55fd147fafd9aeb
GivenBY <suryakantupadhyay18@gmail.com>
2023-12-17 18:05:51 +0530

    feat: menu constants

---
+++ landing-constants.ts
@@ -0,0 +1,14 @@
+export const menus = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Testonomials",
+    href: "#testonomials",
+  },
+  {
+    label: "Contact Us",
+    href: "https://forms.gle/pH53SDvZ91x263bF6",
+  },
+];
