19dddfd16060eafdb5cd7e943ac406f446563fd8
GivenBY <suryakantupadhyay18@gmail.com>
2023-12-17 18:08:51 +0530

    chore: map menu items

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -7,39 +7,41 @@ import { useAuth } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { SheetSide } from "./mobile-navbar";
+
+import { menus } from "@/landing-constants";
 
 const font = Montserrat({ weight: "600", subsets: ["latin"] });
 
 export const LandingNavbar = () => {
   const { isSignedIn } = useAuth();
-  const underline_effect = cn(
-    "relative inline-block group after:bg-blue-500 after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer"
-  );
 
   return (
     <nav className="p-4 flex bg-transparent items-center justify-between ">
       <Link href="/" className="flex items-center">
         <div className="relative h-10 w-10 mr-4">
-          <Image fill alt="" src="/logo.png" />
+          <Image fill alt="logo" src="/logo.png" />
         </div>
         <h1 className={cn("text-2xl font-bold text-white", font.className)}>
           SastaGPT
         </h1>
       </Link>
-      <div className={cn("text-[1.5]xl font-bold text-white", font.className)}>
+      <div
+        className={cn(
+          "text-[1.5]xl font-bold hidden md:block text-white",
+          font.className
+        )}
+      >
         <div className="flex flex-row p-2 gap-6 items-baseline">
-          <Link href="#home" className={underline_effect}>
-            <span className="group-hover:after:w-full">Home</span>
-          </Link>
-          <Link href="#features" className={underline_effect}>
-            <span className="group-hover:after:w-full">Features</span>
-          </Link>
-          <Link href="#faq" className={underline_effect}>
-            <span className="group-hover:after:w-full">FAQ's</span>
-          </Link>
-          <Link href="#contactus" className={underline_effect}>
-            <span className="group-hover:after:w-full">Contact Us</span>
-          </Link>
+          {menus.map((menu) => (
+            <Link
+              key={menu.href}
+              href={menu.href}
+              className="relative inline-block group after:bg-blue-500 after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer"
+            >
+              <span className="group-hover:after:w-full">{menu.label}</span>
+            </Link>
+          ))}
           <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
             <Button
               variant="premium"
@@ -50,6 +52,7 @@ export const LandingNavbar = () => {
           </Link>
         </div>
       </div>
+      <SheetSide />
     </nav>
   );
 };
