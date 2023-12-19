4f3d9d7c118525bd4c38ce2a4c2f7007fe27423b
GivenBY <suryakantupadhyay18@gmail.com>
2023-12-19 12:18:26 +0530

    feat: code api route

---
+++ app/api/code/route.ts
@@ -0,0 +1,42 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import keyopenai from "@/openai-config";
+import { ChatCompletionMessageParam } from "openai/resources/chat/completions";
+
+const instructionMessage: ChatCompletionMessageParam = {
+  role: "system",
+  content:
+    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+};
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!keyopenai.apiKey) {
+      return new NextResponse("OpenAI API Key not configured.", {
+        status: 500,
+      });
+    }
+
+    if (!messages) {
+      return new NextResponse("Messages are required", { status: 400 });
+    }
+
+    const response = await keyopenai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [instructionMessage, ...messages],
+    });
+
+    return NextResponse.json(response.choices[0].message);
+  } catch (error) {
+    console.log("[CODE_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
